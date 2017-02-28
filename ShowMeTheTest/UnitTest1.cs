e5c3d714000f7319e0d02efed66ea1b5c1a5e3af
allanfarrell <afarrell264@hotmail.com>
2017-02-28 15:22:06 +0000

    Unit Test Solution

---
+++ ShowMeTheTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShowMeTheTickets.Controllers;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using ShowMeTheTickets.ViewModels;
+using System.Collections.Generic;
+using ShowMeTheTickets.Infastructure;
+
+namespace ShowMeTheTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SortByPriceTest()
+        {
+            // Arrange
+            List<SearchVM> searchModel = new List<SearchVM>();
+            for(var i = 1; i < 5; i++)
+            {
+                searchModel.Add(new SearchVM(){ PriceValue = 5 - i });
+            }
+
+            // Act
+            List<SearchVM> Sorted = Sorter.SortBy(searchModel, "Price");
+
+            // Assert
+            Assert.AreEqual(searchModel[0].PriceValue, 1);
+        }
+    }
+}
